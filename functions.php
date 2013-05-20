e38a2cbf50e93c2566d1fa70881a413420cfb567
Daniel Sundy <dansundy@gmail.com>
2013-05-19 21:27:05 -0400

    Reorder functions

--- functions.php
+++ functions.php
@@ -4,5 +4,5 @@
  *
  */
 
-require_once( get_template_directory() . '/inc/base-functions.php' );
-require_once( get_template_directory() . '/inc/custom-login.php' );
+require_once( get_template_directory() . '/inc/functions/base-functions.php' );
+require_once( get_template_directory() . '/inc/functions/custom-login.php' );
